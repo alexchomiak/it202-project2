ec579595f066ce416a38e3d7d9357239c498f158
alexchomiak <alex.chomiak@gmail.com>
2019-03-07 03:43:34 -0600

    added prod build

---
+++ precache-manifest.c7c4d4cbd891dc788b16a54bc05d21df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68cd669795ff4b671536",
+    "url": "/static/css/main.186c64c5.chunk.css"
+  },
+  {
+    "revision": "68cd669795ff4b671536",
+    "url": "/static/js/main.68cd6697.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f8a28dd77d48b170af17",
+    "url": "/static/js/2.f8a28dd7.chunk.js"
+  },
+  {
+    "revision": "b40a10d4d4db1368743e44749908bad4",
+    "url": "/index.html"
+  }
+];
